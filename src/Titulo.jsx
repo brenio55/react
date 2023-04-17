990322081caf5840e165f984be6a768badbbbc01
brenio55 <brenio55@outlook.com>
2023-04-17 14:12:54 -0300

    Adicionado input para mudar texto do H1

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 
 function Titulo({ cor }){
     const [texto, setTexto] = useState("Estado inicial") //o primeiro é o valor setado inicialmente, o segundo é o componente que seta o primeiro.
+    const [inputText, setInputText] = useState("")
+
+    function clicou(){
+        setTexto(inputText);
+    }
 
     return (
         <div>
             <h1 style={{ color: cor }}>{texto}</h1>
-            <button onClick={()=>{
-                useState(setTexto("Mudei via botão"))
-            }}>Mudar</button>
+            <input value={inputText} onChange={
+                (e)=>{setInputText(e.target.value)}
+            } type="text" />
+            <button onClick={clicou}>Mudar</button>
         </div>
     )
 }
